788cd73ffff6ec6b8a7891d2cf5a3dd166b453bd
仰鈞 張 <qqwwee2006@gmail.com>
2017-12-04 20:57:39 +0800

    add login page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/admin/login', function () {
+    return view('admin.login');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
