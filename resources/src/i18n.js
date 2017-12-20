017b12ab58b2ae2f63cb04ea1a0bafebe67d52f1
仰鈞 張 <qqwwee2006@gmail.com>
2017-12-20 17:22:58 +0800

    add jp dic

--- resources/src/i18n.js
+++ resources/src/i18n.js
@@ -7,7 +7,7 @@ i18n
   .use(XHR)
   .use(LanguageDetector) // 偵測瀏覽器語系
   .init({
-    fallbackLng: 'en', // 未偵測到時的後備語系
+    fallbackLng: 'zh', // 未偵測到時的後備語系
     ns: ['common'], // 語系的 loading namespace 如語系檔案名稱 common.js
     defaultNS: 'common', // 預設的 namespace name
     debug: true,
