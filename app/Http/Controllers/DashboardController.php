84ba618dd1a25dd75235dc43ef4416cd060c846b
仰鈞 張 <qqwwee2006@gmail.com>
2018-01-04 21:08:06 +0800

    add dashboard view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Hash, Exception;
+use App\Models\Shop;
+use App\Models\Order;
+use App\Models\Service;
 
 class DashboardController extends Controller
 {
@@ -12,7 +15,50 @@ class DashboardController extends Controller
 	public function index()
 	{
 		$view_data = [];
+		$shop_list = Shop::all();
+		foreach ($shop_list as $key => $shop) {
+			
+			$date = date("Y-m-d");
 
+			$first = 1;
+			$w = date('w',strtotime($date));
+			
+			$day_start_time = $date.' '.$shop->start_time;
+			$week_start = date('Y-m-d',strtotime("$date -".($w ? $w - $first : 6).' days'));
+			$week_start_time = $week_start.' '.$shop->start_time;
+			
+			if($shop->end_time <= $shop->start_time){
+				$day_end_time = date('Y-m-d H:i:s', strtotime ("+1 day", strtotime($date.' '.$shop->end_time)));
+				$week_end_time = date('Y-m-d', strtotime("$week_start +7 days")).' '.$shop->end_time;
+			}
+			else{
+				$day_end_time = $date.' '.$shop->end_time;
+				$week_end_time = date('Y-m-d', strtotime("$week_start +6 days")).' '.$shop->end_time;
+			}
+
+			$info['name'] = $shop->name;
+			
+			$day_orders = Order::with('service')->where('shop_id', $shop->id)->where('start_time', '<=', $day_end_time)->where('start_time', '>=', $day_start_time)->get();
+			$info['order_day'] = $day_orders->count();		
+			$info['revenue_day'] = 0;
+			foreach ($day_orders as $key => $order) {
+				if($order->status == 5){
+					$info['revenue_day'] += $order->service->price * $order->person;
+				}
+			}
+
+
+			$week_orders = Order::with('service')->where('shop_id', $shop->id)->where('start_time', '<=', $week_end_time)->where('start_time', '>=', $week_start_time)->get();
+			$info['order_week'] = $week_orders->count();
+			$info['revenue_week'] = 0;
+			foreach ($week_orders as $key => $order) {
+				if($order->status == 5){
+					$info['revenue_week'] += $order->service->price * $order->person;
+				}
+			}
+
+			$view_data['shop_list'][] = $info;
+		}
 		return view('admin.dashboard.index', $view_data);
 	}
 
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -2,6 +2,54 @@
 @section('head')
 @stop
 @section('content')
+<div class="content-page">
+    <!-- Start content -->
+	<div class="content">
+		<div class="container">
+			@foreach($shop_list as $shop)
+			<div class="row">
+				<h1>{{ $shop['name'] }}</h1>
+			</div>
+		    <div class="row">
+                <div class="col-sm-6 col-lg-3">
+                    <div class="widget-simple text-center card-box bg-success">
+                        <h3 class="text-white counter">{{ $shop['order_day'] }}</h3>
+                        <p class="text-white">今日預約</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="widget-simple text-center card-box bg-warning">
+                        <h3 class="text-white counter">{{ $shop['order_week'] }}</h3>
+                        <p class="text-white">一週預約</p>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-lg-3">
+                    <div class="widget-simple text-center card-box bg-pink">
+                        <h3 class="text-white">$ <span class="counter">{{ $shop['revenue_day'] }}</span></h3>
+                        <p class="text-white">今日營業額</p>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-lg-3">
+                    <div class="widget-simple text-center card-box bg-purple">
+                        <h3 class="text-white">$ <span class="counter">{{ $shop['revenue_week'] }}</span></h3>
+                        <p class="text-white">一週營業額</p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+		    <!-- end Panel -->
+		</div>
+	  <!-- end container -->
+	</div>
+	<!-- end content -->
+	<!-- FOOTER -->
+	<footer class="footer text-right">
+	2017 © stevia-network.
+	</footer>
+  <!-- End FOOTER -->
+</div>
 @stop
 @section('script')
 @stop
