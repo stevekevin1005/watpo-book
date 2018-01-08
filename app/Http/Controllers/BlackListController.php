20f2a730ce1923ebc95961c0540d03dec6c02717
仰鈞 張 <qqwwee2006@gmail.com>
2018-01-08 14:35:05 +0800

    add blacklist log

--- app/Http/Controllers/BlackListController.php
+++ app/Http/Controllers/BlackListController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\BlackList;
-use Hash, Exception;
+use App\Models\Log;
+use Hash, Exception; 
 
 class BlackListController extends Controller
 {
@@ -27,11 +28,14 @@ class BlackListController extends Controller
 			$blackList->name = $request->name;
 			$blackList->phone	 = $request->phone;
 			$blackList->save();
-
+			Log::create(['description' => '新增 名字:'.$request->name." 電話:".$request->phone." 黑名單"]);
 			return redirect('/admin/blacklist/list');
 		}
 		catch(Exception $e){
-			return redirect('/admin/blacklist/list')->withErrors(['fail'=>'新增失敗 請洽系統管理商']);
+			return redirect('/admin/blacklist/list')->withErrors(['fail'=>'系統錯誤 請洽系統管理商']);
+		}
+		catch(\Illuminate\Database\QueryException $e){
+			return  redirect('/admin/blacklist/list')->withErrors(['fail'=>'資料庫錯誤 請洽系統管理商']);
 		}
 	}
 
@@ -42,11 +46,14 @@ class BlackListController extends Controller
 			$blackList = new BlackList;
 			$blackList = $blackList->where('id', $request->id)->first();
 			$blackList->delete();
-
+			Log::create(['description' => '移除 名字:'.$blackList->name." 電話:".$blackList->phone." 黑名單"]);
 			return redirect('/admin/blacklist/list');
 		}
 		catch(Exception $e){
-			return redirect('/admin/blacklist/list')->withErrors(['fail'=>'刪除失敗 請洽系統管理商']);
+			return redirect('/admin/blacklist/list')->withErrors(['fail'=>'系統錯誤 請洽系統管理商']);
+		}
+		catch(\Illuminate\Database\QueryException $e){
+			return  redirect('/admin/blacklist/list')->withErrors(['fail'=>'資料庫錯誤 請洽系統管理商']);
 		}
 	}
 
