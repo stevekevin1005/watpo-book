121b45c13e62a565944915dbe3e88cc08d37fd31
仰鈞 張 <qqwwee2006@gmail.com>
2018-01-15 21:54:28 +0800

    add cron schedule

--- config/database.php
+++ config/database.php
@@ -62,6 +62,11 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
+            'dump' => [
+                'dump_binary_path' => '/', // only the path, so without `mysqldump` or `pg_dump`
+                'use_single_transaction',
+                'timeout' => 60 * 5, // 5 minute timeout
+            ]  
         ],
 
         'pgsql' => [
