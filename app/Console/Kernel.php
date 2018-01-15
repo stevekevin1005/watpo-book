fb720c124f750614a379dc39551e450e5aea0888
仰鈞 張 <qqwwee2006@gmail.com>
2018-01-15 23:04:37 +0800

    add clean old file

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('inspire')
                  ->hourly();
-        $schedule->command('backup:run --only-db')->cron('30 5 * * * *');;
+        $schedule->command('backup:run --only-db')->cron('30 5 * * * *');
+        $schedule->command('backup:clean')->cron('30 5 * * * *');
     }
 }
