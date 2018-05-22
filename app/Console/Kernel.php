fd3e6406f48d1e5c1747d6c9989a954caff312fc
AreyLiu <areyliu6@gmail.com>
2018-05-22 19:10:14 +0800

    schedule controller call

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('inspire')
                  ->hourly();
         $schedule->call('App\Http\Controllers\SmsController@schedulingSendReportSMS')->everyMinute();
+        $schedule->call('App\Http\Controllers\ReportController@FinishedService')->everyMinute();
         $schedule->command('backup:run --only-db')->dailyAt('05:00');
         $schedule->command('backup:clean')->dailyAt('05:00');
     }
